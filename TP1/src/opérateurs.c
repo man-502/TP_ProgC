e36aab285009bc1a9c969afcc9600bf4bbeff9ba
man-502 <makab76@hotmail.fr>
2025-12-03 20:42:44 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -5,14 +5,12 @@ int main(void) {
     int a = 16;
     int b = 3;
 
-    /* Opérations arithmétiques */
     printf("a + b = %d\n", a + b);
     printf("a - b = %d\n", a - b);
     printf("a * b = %d\n", a * b);
     printf("a / b = %d\n", a / b);
     printf("a %% b = %d\n", a % b);
 
-    /* Opérateurs logiques / comparaisons */
     printf("a == b : %d\n", a == b);
     printf("a >  b : %d\n", a > b);
 
