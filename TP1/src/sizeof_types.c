10511d2bb80345b2395b1a5ff9c754e8913b5e27
man-502 <makab76@hotmail.fr>
2025-12-03 20:25:43 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    printf("Taille de char : %zu octets\n", sizeof(char));
-    printf("Taille de signed char : %zu octets\n", sizeof(signed char));
-    printf("Taille de unsigned char : %zu octets\n\n", sizeof(unsigned char));
 
-    printf("Taille de short : %zu octets\n", sizeof(short));
-    printf("Taille de signed short : %zu octets\n", sizeof(signed short));
-    printf("Taille de unsigned short : %zu octets\n\n", sizeof(unsigned short));
+    printf("Taille de char : %lu octets\n", (unsigned long) sizeof(char));
+    printf("Taille de signed char : %lu octets\n", (unsigned long) sizeof(signed char));
+    printf("Taille de unsigned char : %lu octets\n\n", (unsigned long) sizeof(unsigned char));
 
-    printf("Taille de int : %zu octets\n", sizeof(int));
-    printf("Taille de signed int : %zu octets\n", sizeof(signed int));
-    printf("Taille de unsigned int : %zu octets\n\n", sizeof(unsigned int));
+    printf("Taille de short : %lu octets\n", (unsigned long) sizeof(short));
+    printf("Taille de signed short : %lu octets\n", (unsigned long) sizeof(signed short));
+    printf("Taille de unsigned short : %lu octets\n\n", (unsigned long) sizeof(unsigned short));
 
-    printf("Taille de long int : %zu octets\n", sizeof(long int));
-    printf("Taille de signed long int : %zu octets\n", sizeof(signed long int));
-    printf("Taille de unsigned long int : %zu octets\n\n", sizeof(unsigned long int));
+    printf("Taille de int : %lu octets\n", (unsigned long) sizeof(int));
+    printf("Taille de signed int : %lu octets\n", (unsigned long) sizeof(signed int));
+    printf("Taille de unsigned int : %lu octets\n\n", (unsigned long) sizeof(unsigned int));
 
-    printf("Taille de long long int : %zu octets\n", sizeof(long long int));
-    printf("Taille de signed long long int : %zu octets\n", sizeof(signed long long int));
-    printf("Taille de unsigned long long int : %zu octets\n\n", sizeof(unsigned long long int));
+    printf("Taille de long int : %lu octets\n", (unsigned long) sizeof(long int));
+    printf("Taille de signed long int : %lu octets\n", (unsigned long) sizeof(signed long int));
+    printf("Taille de unsigned long int : %lu octets\n\n", (unsigned long) sizeof(unsigned long int));
 
-    printf("Taille de float : %zu octets\n\n", sizeof(float));
+    printf("Taille de long long int : %lu octets\n", (unsigned long) sizeof(long long int));
+    printf("Taille de signed long long int : %lu octets\n", (unsigned long) sizeof(signed long long int));
+    printf("Taille de unsigned long long int : %lu octets\n\n", (unsigned long) sizeof(unsigned long long int));
 
-    printf("Taille de double : %zu octets\n\n", sizeof(double));
-
-    printf("Taille de long double : %zu octets\n\n", sizeof(long double));
+    printf("Taille de float : %lu octets\n\n", (unsigned long) sizeof(float));
+    printf("Taille de double : %lu octets\n\n", (unsigned long) sizeof(double));
+    printf("Taille de long double : %lu octets\n\n", (unsigned long) sizeof(long double));
 
     return 0;
 }
-
-
