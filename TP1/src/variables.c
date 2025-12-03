8132a31af8f855f7b391b89bbec8945b55b403b0
man-502 <makab76@hotmail.fr>
2025-12-03 20:37:32 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -1,55 +1,45 @@
 #include <stdio.h>
 
-int main() {
-
-    /* Types char */
+int main(void) {
     char c = 'A';
     signed char sc = -10;
     unsigned char uc = 200;
 
-    /* Types short */
     short s = 123;
-    signed short ss = -123;
     unsigned short us = 456;
 
-    /* Types int */
     int i = 1000;
-    signed int si = -1000;
     unsigned int ui = 3000;
 
-    /* Types long int */
-    long int li = 100000L;
-    signed long int sli = -100000L;
-    unsigned long int uli = 200000UL;
+    long li = 100000L;
+    unsigned long uli = 200000UL;
 
-    /* Types long long int */
-    long long int lli = 1000000000LL;
-    signed long long int slli = -1000000000LL;
-    unsigned long long int ulli = 2000000000ULL;
+    long long lli = 1000000000LL;
+    unsigned long long ulli = 2000000000ULL;
 
-    /* Types flottants */
     float f = 3.14f;
     double d = 2.718281828;
     long double ld = 1.23456789012345L;
 
-    /* Affichage */
     printf("char = %c\n", c);
     printf("signed char = %d\n", sc);
-    printf("unsigned char = %u\n\n", uc);
+    printf("unsigned char = %u\n", uc);
 
     printf("short = %d\n", s);
-    printf("signed short = %d\n", ss);
-    printf("unsigned short = %u\n\n", us);
+    printf("unsigned short = %u\n", us);
 
     printf("int = %d\n", i);
-    printf("signed int = %d\n", si);
-    printf("unsigned int = %u\n\n", ui);
+    printf("unsigned int = %u\n", ui);
 
-    printf("long int = %ld\n", li);
-    printf("signed long int = %ld\n", sli);
-    printf("unsigned long int = %lu\n
+    printf("long = %ld\n", li);
+    printf("unsigned long = %lu\n", uli);
 
+    printf("long long = %lld\n", lli);
+    printf("unsigned long long = %llu\n", ulli);
 
+    printf("float = %f\n", f);
+    printf("double = %lf\n", d);
+    printf("long double = %Lf\n", ld);
 
-
-
+    return 0;
+}
