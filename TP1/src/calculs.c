e5d54c68e23d4062c6bf131b3665ebabbd4c2804
man-502 <makab76@hotmail.fr>
2025-12-03 20:52:55 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int num1 = 12;
+    int num2 = 5;
+    char op = '&';  /* Changez cette valeur pour tester */
+
+    switch (op) {
+
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            else
+                printf("Erreur : division par zéro !\n");
+            break;
+
+        case '%':
+            if (num2 != 0)
+                printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            else
+                printf("Erreur : modulo par zéro !\n");
+            break;
+
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+
+        default:
+            printf("Opérateur inconnu : %c\n", op);
+            break;
+    }
+
+    return 0;
+}
+
 
