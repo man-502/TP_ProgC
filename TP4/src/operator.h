30c0673c19c999d0163593ab28b8d3aeae5f6d59
man-502 <makab76@hotmail.fr>
2025-12-17 21:14:37 +0100

    Update operator.h

--- TP4/src/operator.h
+++ TP4/src/operator.h
@@ -1,14 +1,15 @@
 #ifndef OPERATOR_H
 #define OPERATOR_H
 
-/* Prototypes des fonctions de calcul */
 int somme(int a, int b);
 int difference(int a, int b);
 int produit(int a, int b);
 int quotient(int a, int b);
 int modulo(int a, int b);
-int et_bitwise(int a, int b);
-int ou_bitwise(int a, int b);
+int et(int a, int b);
+int ou(int a, int b);
 int negation(int a);
 
 #endif
+
+
