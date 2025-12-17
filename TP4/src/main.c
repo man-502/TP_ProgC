0fca47d3f0b8fab2efdb37284aae7d9729f8f342
man-502 <makab76@hotmail.fr>
2025-12-17 20:11:45 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -13,23 +13,33 @@ int main() {
         printf("2. Écrire dans un fichier\n");
         printf("3. Quitter\n");
         printf("Votre choix : ");
-        scanf("%d", &choix);
-        getchar(); // consommer le '\n' restant
+
+        if (scanf("%d", &choix) != 1) {
+            printf("Entrée invalide !\n");
+            // Vider le buffer
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }
+
+        // Vider le buffer après scanf pour éviter le problème de curseur blanc
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
 
         if (choix == 1) {
             printf("Entrez le nom du fichier à lire : ");
             fgets(nom_fichier, sizeof(nom_fichier), stdin);
-            nom_fichier[strcspn(nom_fichier, "\n")] = '\0'; // supprimer le '\n'
+            nom_fichier[strcspn(nom_fichier, "\n")] = '\0'; // supprimer '\n'
             lire_fichier(nom_fichier);
 
         } else if (choix == 2) {
             printf("Entrez le nom du fichier dans lequel vous souhaitez écrire : ");
             fgets(nom_fichier, sizeof(nom_fichier), stdin);
-            nom_fichier[strcspn(nom_fichier, "\n")] = '\0'; // supprimer le '\n'
+            nom_fichier[strcspn(nom_fichier, "\n")] = '\0'; // supprimer '\n'
 
             printf("Entrez le message à écrire : ");
             fgets(message, sizeof(message), stdin);
-            message[strcspn(message, "\n")] = '\0'; // supprimer le '\n'
+            message[strcspn(message, "\n")] = '\0'; // supprimer '\n'
 
             ecrire_dans_fichier(nom_fichier, message);
 
@@ -46,4 +56,5 @@ int main() {
 
     return 0;
 }
+
 
