a74de750f7f1075bbcafeef4d25523e5b3ebce94
man-502 <makab76@hotmail.fr>
2025-12-17 20:04:06 +0100

    Update fichier.h

---
+++ TP4/src/fichier.h
@@ -0,0 +1,10 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+
+/* Fonction pour lire le contenu d'un fichier */
+void lire_fichier(const char *nom_de_fichier);
+
+/* Fonction pour écrire un message dans un fichier */
+void ecrire_dans_fichier(const char *nom_de_fichier, const char *message);
+
+#endif
