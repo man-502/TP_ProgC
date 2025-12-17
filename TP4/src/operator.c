3c0746bb13047abec61934bfad6f7f9d7ac4bc51
man-502 <makab76@hotmail.fr>
2025-12-17 21:15:03 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,15 +1,11 @@
-#ifndef OPERATOR_H
-#define OPERATOR_H
+#include "operator.h"
 
-int somme(int a, int b);
-int difference(int a, int b);
-int produit(int a, int b);
-int quotient(int a, int b);
-int modulo(int a, int b);
-int et(int a, int b);
-int ou(int a, int b);
-int negation(int a);
-
-#endif
-
+int somme(int a, int b) { return a + b; }
+int difference(int a, int b) { return a - b; }
+int produit(int a, int b) { return a * b; }
+int quotient(int a, int b) { return a / b; } // vérification division par 0 dans main
+int modulo(int a, int b) { return a % b; }
+int et(int a, int b) { return a & b; }
+int ou(int a, int b) { return a | b; }
+int negation(int a) { return ~a; }
 
